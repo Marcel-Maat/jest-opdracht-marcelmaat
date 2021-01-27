591bc22117b7282694f01375f4a18f57f5f479f3
unknown <marcel.maat@gmail.com>
2021-01-27 17:13:30 +0100

    remove solutions

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-function multiplyDivide(num1, num2, num3) {
-    console.log("INPUT", num1, num2, num3);
-    const result = num1 * num2 / num3;
-    console.log("RESULT", result);
-    return result
-};
-multiplyDivide(10, 10, 100);
+function multiplyDivide() {
+   //1. Vermenigvuldig 2 getallen en deel het door een 3e getal. De uitkomst moet 1 zijn.
+    //2. Er zijn meerdere antwoorden mogelijk, wil je op hetzelfde uitkomen als ik, lees 3.
+    //3. De eerste 2 getallen zijn hoger dan 5 maar lager dan 11, het derde getal is hoger dan 90 en lager dan 105
+
+
+}
+multiplyDivide();
 
 module.exports = {
     multiplyDivide: multiplyDivide,
